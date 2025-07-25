97c09185e2a6ce9bb73e63851479b6f4b3b82f81
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-07-25 10:59:25 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDYrcJmNxZcHWHH5_ra0P9lqQMIOqOqE0g",
-  authDomain: "orbit-technology.firebaseapp.com",
-  projectId: "orbit-technology",
-  storageBucket: "orbit-technology.appspot.com",
-  messagingSenderId: "581040210640",
-  appId: "1:581040210640:web:3fc54f8f4d5c5d5d5c5d5c",
-  measurementId: "G-XXXXXXXXXX"
+  apiKey: "AIzaSyA6YFQ0Sdwwrz1dD6ot41j4x0PD6yBSdsU",
+  authDomain: "orbittechn-93fb1.firebaseapp.com",
+  projectId: "orbittechn-93fb1",
+  storageBucket: "orbittechn-93fb1.firebasestorage.app",
+  messagingSenderId: "858990599268",
+  appId: "1:858990599268:web:fcabdd90433e7310be5371",
+  measurementId: "G-5BY6KEZWPF"
 };
 
 const app = initializeApp(firebaseConfig);
